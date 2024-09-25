04be13d35298980851bcd78f52287819e5f2d0e6
kkkunny <1401289859@qq.com>
2024-09-25 08:56:01 +0800

    支持设置代理

--- register.go
+++ register.go
@@ -1,6 +1,10 @@
 package bot
 
 import (
+	"net/http"
+	"net/url"
+
+	"github.com/2mf8/Go-QQ-Client/internal/proxy"
 	"github.com/2mf8/Go-QQ-Client/log"
 	"github.com/2mf8/Go-QQ-Client/openapi"
 	"github.com/2mf8/Go-QQ-Client/websocket"
@@ -24,4 +28,13 @@ func SetWebsocketClient(c websocket.WebSocket) {
 // SetOpenAPIClient 注册 openapi 的不同实现，需要设置版本
 func SetOpenAPIClient(v openapi.APIVersion, c openapi.OpenAPI) {
 	openapi.Register(v, c)
+}
+
+// SetProxy 设置代理
+//
+// 若调用了SetWebsocketClient，会导致websocket代理无效，请自行设置websocket代理
+//
+// 若无设置，则使用系统代理
+func SetProxy(p func(*http.Request) (*url.URL, error)) {
+	proxy.Proxy = p
 }
